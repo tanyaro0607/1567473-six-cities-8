492cf566fbb06dfece6cf5088f888ee731a3aa83
Tanya <ta.rohlova@yandex.ru>
2021-10-03 19:59:46 +0300

    отрисовываю компонет App

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,12 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({ offersCount }: AppScreenProps): JSX.Element {
+  return <MainPage offersCount={offersCount} />;
 }
 
 export default App;
+
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  OFFERS_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      offersCount = {Setting.OFFERS_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
